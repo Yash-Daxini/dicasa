2a537399dc10588d4d37cf6171477599cf63bfd7
Infinity <200540107113@darshan.ac.in>
2023-11-02 17:23:28 +0530

    Home Page completed

--- furni/js/custom.js
+++ furni/js/custom.js
@@ -73,4 +73,15 @@
 })()
 
 
-
+jQuery(document).ready(function($) {
+	$(window).scroll(function() {
+	  var scrollPos = $(window).scrollTop(),
+		navbar = $('.navbar');
+  
+	  if (scrollPos > 50) {
+		navbar.addClass('alt-color');
+	  } else {
+		navbar.removeClass('alt-color');
+	  }
+	});
+  });
